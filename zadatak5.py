53186f0093da7d77c9b0af19ecda946481e85682
karlobosnjak <kbosnjak@pmfst.hr>
2021-03-18 22:52:31 +0100

    Zadatak5 ispravak

--- zadatak5.py
+++ zadatak5.py
@@ -4,6 +4,8 @@ def graf(x1,x2,y1,y2):
     x = [x1,x2]
     y = [y1,y2]
     plt.plot(x,y)
+    plt.plot(x,y,'ro')
+    plt.plot()
     prikaz = int(input('Ako zelite odmah prikzati graf pritisnite 1,ako zelite graf u PDFu pritisnite 2:'))
     if prikaz == 1:
         plt.show()
